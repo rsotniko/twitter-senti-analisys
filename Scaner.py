baa0b565208f24638ad1ed6112ccde745e4f4058
drdolgosheev <dmitriy.dolgosheev@yandex.ru>
2018-12-31 16:47:22 +0300

    Без исключений

--- Scaner.py
+++ Scaner.py
@@ -1,22 +1,38 @@
-class Rectangle:
+"""
+Файл 123 содержит данные для проверки парсера
+"""
+
+
+class Data:
     user = " "
     theme = " "
     mainTxt = " "
 
-    def med(self):
-        return 10
+    def Parse(self, stream):
+        _str = Data()
+        stream = stream.replace(" ", "", stream.__sizeof__())
+        _str.user = stream.split(';', 3)[0]
+        _str.theme = stream.split(';', 3)[1]
+        _str.mainTxt = stream.split(';', 3)[2]
+        return _str
+
+    @staticmethod
+    def ParseFullFile(_fileName):
+        _list = []
+        f = open(_fileName)
+        text = f.readline()
+        while text:
+            _r = Data()
+            _r = _r.Parse(text)
+            _list.append(_r)
+            text = f.readline()
+        f.close()
+        return _list
 
 
-_list = []
-f = open('123')
-
-text = f.readline()
-while text:
-    _r = Rectangle()
-    _list.append(_r)
-    print(text, end='')
-    text = f.readline()
-
-f.close()
-_r = Rectangle()
-print('\n' + str(_r.med()))
+# Для проверки работоспособности
+_list = Data.ParseFullFile('123')
+print(_list[1].user)
+print(_list[1].mainTxt)
+print(_list[2].user)
+print(_list[2].mainTxt)
