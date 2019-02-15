cb724bd37047aaa10a852b2835ad44368d0e3fb3
dimadolgosheev <drdolgosheev@edu.hse.ru>
2019-02-15 19:28:16 +0300

    Frame + testButton

--- MainWorkflow/Display.py
+++ MainWorkflow/Display.py
@@ -1,12 +1,42 @@
-class Display(object):
+import tkinter as tk
+from Tkinter import Tk, BOTH
+from ttk import Frame, Button, Style
 
-    def __init__(self, saver_data):
-        self.data = saver_data
-        # We need a parser here, to parse our result
-        # self.frame1 = Parse(saver_data)
-        # self.frame2 = Parse(saver_data)
 
-    # main method to display
-    @staticmethod
-    def display(param):
-        return 0
+class Display(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.style = Style()
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        self.parent.title("Quit button")
+        self.style.theme_use("default")
+
+        self.pack(fill=BOTH, expand=1)
+
+        quitButton = Button(self, text="Quit", command=self.quit)
+        quitButton.place(x=1, y=1)
+
+    def centerWindow(self):
+        w = 700
+        h = 450
+
+        sw = self.parent.winfo_screenwidth()
+        sh = self.parent.winfo_screenheight()
+
+        x = (sw - w) / 2
+        y = (sh - h) / 2
+        self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+
+def main():
+    root = Tk()
+    app = Display(root)
+    app.centerWindow()
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
