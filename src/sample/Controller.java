59852f12add0fc5b92f36008d8413e0b59feba30
dimadolgosheev <drdolgosheev@edu.hse.ru>
2019-03-26 23:18:41 +0300

    ..

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField num_txt;
+
+    @FXML
+    private TextField hashtag_txt;
+
+    @FXML
+    private Button run_button;
+
+    @FXML
+    void initialize() {
+        assert num_txt != null : "fx:id=\"num_txt\" was not injected: check your FXML file 'sample.fxml'.";
+        assert hashtag_txt != null : "fx:id=\"hashtag_txt\" was not injected: check your FXML file 'sample.fxml'.";
+        assert run_button != null : "fx:id=\"run_button\" was not injected: check your FXML file 'sample.fxml'.";
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Analyzer");
+        primaryStage.setScene(new Scene(root, 493, 352));
         primaryStage.show();
     }
 
